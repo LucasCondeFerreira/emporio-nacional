582ac3105886d6b893bb21b8fcbfeef7fe7c81e5
Lucas C <LucasCondeFerreira@gmail.com>
2022-12-19 15:18:04 -0300

    Finish -v 1.0.5

---
+++ enviarEmail.php
@@ -0,0 +1,31 @@
+<?php
+//1 – Definimos Para quem vai ser enviado o email
+$para = "jorneyunderworld@gmail.com";
+//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
+$name = $_POST['nameInput'];
+$number = $_POST['numberInput'];
+$email = $_POST['emailInput'];
+// 3 - resgatar o assunto digitado no formulário e  grava na variavel
+//$assunto
+$assunto = $_POST['assunto'];
+ //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+$mensagem = "<strong>Boa tarde gostaria de entrar em contato. <br></strong>";
+$mensagem .= "Esses sãos meus dados: <br>";
+$mensagem .= "<br><br>  <strong>Nome: </strong>".name;
+$mensagem .= "<br><br>  <strong>Número: </strong>".number;
+$mensagem .= "<br><br>  <strong>E-mail: </strong>".email;
+
+//5 – agora inserimos as codificações corretas e  tudo mais.
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+$headers .= "From:  dominio.com.br<sistema@dominio.com.br>\n";
+//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+$headers .= "X-Sender:  <sistema@dominio.com.br>\n";
+//email do servidor //que enviou
+$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+$headers .= "Return-Path:  <sistema@dominio.com.br>\n";
+//caso a msg //seja respondida vai para  este email.
+$headers .= "MIME-Version: 1.0\n";
+
+mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
+?>
